558d5647085e18bef4ea325133b8e8ba6db70e06
Matheus Malta <mfsm@cin.ufpe.br>
2026-05-07 19:47:09 -0300

    fix: change load_dotenv to override=False to respect container env vars

--- pipeline/config.py
+++ pipeline/config.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv(override=True)
+load_dotenv(override=False)
 
 PROJECT_ID        = os.environ["GCP_PROJECT_ID"]
 DATASET_ID        = os.environ["GCP_DATASET_ID"]
