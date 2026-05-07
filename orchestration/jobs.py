f95b2ec910865ac5b169ab5fa018609e8a587928
Matheus Malta <mfsm@cin.ufpe.br>
2026-05-07 19:46:26 -0300

    feat: set in_process_executor on pipeline job

--- orchestration/jobs.py
+++ orchestration/jobs.py
@@ -1,6 +1,7 @@
-from dagster import define_asset_job, AssetSelection
+from dagster import define_asset_job, AssetSelection, in_process_executor
 
 github_pipeline_job = define_asset_job(
     name="github_pipeline_job",
     selection=AssetSelection.all(),
+    executor_def=in_process_executor,
 )
