66665ff1bd22b7fa80fa9b53b011af7add4e2df0
Matheus Malta <mfsm@cin.ufpe.br>
2026-05-31 08:35:47 -0300

    fix: remove service account key creation from Terraform

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -18,18 +18,8 @@ resource "google_project_iam_member" "bq_job_user" {
   member  = "serviceAccount:${google_service_account.pipeline_sa.email}"
 }
 
-# 4. Gera a "chave" (o arquivo .json) para usar no Python
-resource "google_service_account_key" "pipeline_sa_key" {
-  service_account_id = google_service_account.pipeline_sa.name
-}
 
-# 5. Mostra a chave no final
-output "service_account_key" {
-  value     = google_service_account_key.pipeline_sa_key.private_key
-  sensitive = true
-}
-
-# 6. Dá permissão de Admin de Objetos no Bucket para o Robô
+# 4. Dá permissão de Admin de Objetos no Bucket para o Robô
 resource "google_storage_bucket_iam_member" "sa_storage_admin" {
   bucket = google_storage_bucket.airbyte_state_bucket.name
   role   = "roles/storage.objectAdmin"
